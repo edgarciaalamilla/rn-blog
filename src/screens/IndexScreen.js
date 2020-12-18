cabbdb3e58964fcb55a042f614b1544d4215ff4b
edgarciaalamilla <eddiegarcia.cs@gmail.com>
2020-12-17 23:41:10 -0600

    Update BlogContext to handle blog titles
    Add blogPosts state to handle list of blog posts in BlogContext
    
    Add addBlogPost function to add new blog posts to blogPosts context.
    
    Update IndexScreen to show list of blog posts from blogContext.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,15 +1,26 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
+  /*  When you use a function passed through context,
+      apparently you do not need to pass in an arrow function.
+      just call it by its name, like the way done in the button onPress.
+  */
 
   return (
     <View>
       <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
